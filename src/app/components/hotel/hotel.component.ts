d537bdc0b7767370a3c9f60338cf9313bbf5236b
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2020-10-14 22:18:44 +0600

    Update hotel.component.ts

--- src/app/components/hotel/hotel.component.ts
+++ src/app/components/hotel/hotel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-hotel',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HotelComponent implements OnInit {
 
   public CData: number;
+  @Input() PData: number;
   constructor() { }
 
   ngOnInit(): void {
