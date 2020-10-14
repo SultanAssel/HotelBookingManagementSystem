bced2dc85f1736fa7d38b2cdc2f33f6b404d32be
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2020-10-14 22:39:57 +0600

    Update add-comments.component.ts

--- src/app/components/hotel/add-comments.component.ts
+++ src/app/components/hotel/add-comments.component.ts
@@ -6,17 +6,17 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class AddCommentsComponent implements OnInit {
   @Input() PData: number;
+  public CData: number;
   @Output() childEvent = new EventEmitter();
   constructor() { }
+  // tslint:disable-next-line:typedef
   onChange(value) {
     this.childEvent.emit(value);
 
   }
+  // tslint:disable-next-line:typedef
   ngOnInit() {
   }
 
 }
-
-
-
 
