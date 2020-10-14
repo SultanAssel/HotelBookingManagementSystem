d7750050a8ee6d27841c3ac063e2404261caf13d
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2020-10-14 20:49:36 +0600

    Update contact.component.ts

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 
+
 import { Component, OnChanges, Input, OnInit, DoCheck,
   AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
@@ -9,18 +10,21 @@ import { Component, OnChanges, Input, OnInit, DoCheck,
 })
 export class ContactComponent implements  OnChanges, OnInit, DoCheck,
   AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
-@Input() message="";
+@Input() message = '';
+  firstname: any;
+  email: any;
+  YourMessage: any;
   constructor() {
-    console.log("ChildComponent:Constructor");
+    console.log('ChildComponent:Constructor');
   }
-  ngOnChanges() { console.log("ChildComponent:OnChanges"); }
-  ngOnInit() { console.log("ChildComponent:OnInit"); }
-  ngDoCheck() { console.log("ChildComponent:DoCheck"); }
-  ngAfterContentInit() { console.log("ChildComponent:AfterContentInit"); }
-  ngAfterContentChecked(){console.log("ChildComponent:AfterContentChecked");}
-  ngAfterViewInit() { console.log("ChildComponent:AfterViewInit"); }
-  ngAfterViewChecked() { console.log("ChildComponent:AfterViewChecked"); }
-  ngOnDestroy() { console.log("ChildComponent:OnDestroy"); }
+  ngOnChanges() { console.log('ChildComponent:OnChanges'); }
+  ngOnInit() { console.log('ChildComponent:OnInit'); }
+  ngDoCheck() { console.log('ChildComponent:DoCheck'); }
+  ngAfterContentInit() { console.log('ChildComponent:AfterContentInit'); }
+  ngAfterContentChecked(){console.log('ChildComponent:AfterContentChecked');}
+  ngAfterViewInit() { console.log('ChildComponent:AfterViewInit'); }
+  ngAfterViewChecked() { console.log('ChildComponent:AfterViewChecked'); }
+  ngOnDestroy() { console.log('ChildComponent:OnDestroy'); }
 }
 
 
